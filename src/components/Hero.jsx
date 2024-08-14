5e74d1bd1d742982273fa775a8133fd9adfcbc72
vitthal-07 <vitthalbiradar50@gmail.com>
2024-08-14 18:48:26 +0530

    added youtube logo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,7 @@ import heroImage from "../assets/hero.png";
 import apple from "../assets/Apple-Prodcast.svg";
 import google from "../assets/Google-Prodcast.svg";
 import spotify from "../assets/Spotify.svg";
+import youtube from "../assets/Youtube.svg";
 
 const Hero = () => {
     return (
@@ -10,9 +11,50 @@ const Hero = () => {
             <div className='flex items-center justify-center mt-5 h-16'>
                 <h2 className='text-3xl font-bold'>Available on:</h2>
                 <div className='ml-3 flex items-center justify-between gap-4'>
-                    <img src={google} alt='' className='h-10 object-cover' />
-                    <img src={apple} alt='' className='h-10 object-cover' />
-                    <img src={spotify} alt='' className='h-10 object-cover' />
+                    <a
+                        href='https://podcasts.google.com/'
+                        target='_blank'
+                        rel='noopener noreferrer'
+                    >
+                        <img
+                            src={google}
+                            alt='Google Podcast'
+                            className='h-10 object-cover'
+                        />
+                    </a>
+                    <a
+                        href='https://podcasts.apple.com/'
+                        target='_blank'
+                        rel='noopener noreferrer'
+                    >
+                        <img
+                            src={apple}
+                            alt='Apple Podcast'
+                            className='h-10 object-cover'
+                        />
+                    </a>
+                    <a
+                        href='https://www.spotify.com/'
+                        target='_blank'
+                        rel='noopener noreferrer'
+                    >
+                        <img
+                            src={spotify}
+                            alt='Spotify'
+                            className='h-10 object-cover'
+                        />
+                    </a>
+                    <a
+                        href='https://www.youtube.com/'
+                        target='_blank'
+                        rel='noopener noreferrer'
+                    >
+                        <img
+                            src={youtube}
+                            alt='YouTube'
+                            className='h-10 object-cover'
+                        />
+                    </a>
                 </div>
             </div>
         </div>
