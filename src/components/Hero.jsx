b51ae3fea647da4bc46d7ead744ace85b968acc9
Navjeevan Alone <navjeevanalone352@gmail.com>
2024-08-16 20:38:04 +0530

    image changed hidden bars on small devices

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import heroImage from "../assets/hero.png";
+import heroImage from "../assets/hero-without-bars.png";
 import spotify from "../assets/Spotify.svg";
 import youtube from "../assets/Youtube.svg";
 import video from "../assets/soundwaves.mp4"; // Replace with your video path
@@ -9,35 +9,35 @@ const Hero = () => {
     <div className="relative w-full mt-8 h-[75vh] overflow-hidden">
       {/* Background Video */}
       <video
-        className="absolute top-0 left-0 w-full h-full object-cover opacity-15 pointer-events-none"
+        className="hidden absolute top-0 left-0 w-full h-full object-cover opacity-15 pointer-events-none md:block"
         src={video}
         autoPlay
         muted
         loop></video>
 
       {/* Foreground Image */}
-      <div className="relative z-10 flex flex-col items-center justify-center h-full">
+      <div className="relative z-10 flex flex-col items-center justify-center w-full max-h-[25rem]">
         <img
           src={heroImage}
-          className="object-cover max-h-[75vh] w-full"
+          className="object-contain max-h-[50vh] w-full"
           alt="Hero"
         />
-        <div className="flex items-center justify-center mt-5 h-16">
-          <h2 className="text-3xl font-bold text-white">Available on:</h2>
-          <div className="ml-3 flex items-center gap-4">
-            <a
-              href="https://www.spotify.com/"
-              target="_blank"
-              rel="noopener noreferrer">
-              <img src={spotify} alt="Spotify" className="h-10 object-cover" />
-            </a>
-            <a
-              href="https://www.youtube.com/"
-              target="_blank"
-              rel="noopener noreferrer">
-              <img src={youtube} alt="YouTube" className="h-10 object-cover" />
-            </a>
-          </div>
+      </div>
+      <div className="flex items-center justify-center mt-5 h-16">
+        <h2 className="text-3xl font-bold text-white">Available on:</h2>
+        <div className="ml-3 flex items-center gap-4">
+          <a
+            href="https://www.spotify.com/"
+            target="_blank"
+            rel="noopener noreferrer">
+            <img src={spotify} alt="Spotify" className="h-10 object-cover" />
+          </a>
+          <a
+            href="https://www.youtube.com/"
+            target="_blank"
+            rel="noopener noreferrer">
+            <img src={youtube} alt="YouTube" className="h-10 object-cover" />
+          </a>
         </div>
       </div>
     </div>
