659f830d86da37e0c850e10ff0d1f806833963a8
vitthal-07 <vitthalbiradar50@gmail.com>
2024-08-27 13:00:22 +0530

    Revert "Changed blog config"
    This reverts commit 0d2e38921d4b2c758767c31f3d26b934c68932c2.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,16 @@
-import { useGetBlogs } from '../api/BlogApi';
-import { useGetTeamMembers } from '../api/TeamMemberApi';
-import CategoriesList from '../components/CategoriesList';
-import Footer from '../components/Footer';
-import Header from '../components/Header';
-import Hero from '../components/Hero';
-import LatestBlogs from '../components/LatestBlogs';
-import LatestEpisodesSection from '../components/LatestEpisodesSection';
-import OurTeamSection from '../components/OurTeamSection';
-import PodcastOverview from '../components/PodcastOverview';
-import SubsciptionSection from '../components/SubsciptionSection';
-import TopPodcast from '../components/TopPodcast';
+
+import { useGetBlogs } from "../api/BlogApi";
+import { useGetTeamMembers } from "../api/TeamMemberApi";
+import PostList from "../components/BlogCard2";
+import CategoriesList from "../components/CategoriesList";
+import Hero from "../components/Hero";
+import LatestBlogs from "../components/LatestBlogs";
+import LatestEpisodesSection from "../components/LatestEpisodesSection";
+import OurTeamSection from "../components/OurTeamSection";
+import PodcastOverview from "../components/PodcastOverview";
+import SubsciptionSection from "../components/SubsciptionSection";
+import TopPodcast from "../components/TopPodcast";
+import LoadingSpinner from "./../components/LoadingSpinner";
 
 const HomePage = () => {
     const { blogs, loading: blogsLoading } = useGetBlogs();
