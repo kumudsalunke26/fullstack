fc184bcca5d27eccc6606b73eebfac13cb798443
github <kumudsalunke26@gmail.com>
2025-01-01 19:24:29 +0530

    commands modifications

--- server/script.js
+++ server/script.js
@@ -24,7 +24,7 @@ app.use(cookieParser())
 
 app.use(cors(
   {
-  origin: "http://localhost:5174", // Replace with your frontend's URL
+  origin: "http://localhost:5173", // Replace with your frontend's URL
   credentials: true, // Allow cookies and authentication headers
 }
 ));
