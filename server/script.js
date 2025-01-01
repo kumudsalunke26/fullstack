0f1be6a6f99fdc07318d9d420d94049bb3f7c0a9
github <kumudsalunke26@gmail.com>
2025-01-01 22:33:54 +0530

    changes

--- server/script.js
+++ server/script.js
@@ -88,7 +88,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
 app.use(cors({
-  origin: "https://fullstack-4-jdac.onrender.com/", // Replace with your frontend's URL
+  origin: "https://fullstack-4-jdac.onrender.com", // Replace with your frontend's URL
   credentials: true, // Allow cookies and authentication headers
 }));
 
